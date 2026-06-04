0f9672d9f073836383b2c3f7e5aa63f8a556eb36
w3oodh40-a11y <w3oodh40@gmail.com>
2026-06-04 03:00:23 +0300

    1

--- Assets/Waad/Scripts/CameraFollow.cs
+++ Assets/Waad/Scripts/CameraFollow.cs
@@ -3,10 +3,25 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
+    public Vector3 offset = new Vector3(0, 2.2f, -2.5f);
+    public float mouseSensitivity = 3f;
+
+    float yaw = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
 
     void LateUpdate()
     {
-        transform.position = target.position + new Vector3(0, 2.2f, -2.5f);
+        yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
+
+        Quaternion rotation = Quaternion.Euler(0, yaw, 0);
+
+        transform.position = target.position + rotation * offset;
+
         transform.LookAt(target.position + Vector3.up);
     }
 }
